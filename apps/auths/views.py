78ec434f8354d5000acff2792a508aa3e858a083
Ridzen <ridze00@gmail.com>
2023-06-14 09:27:30 -0500

    for polling

--- apps/auths/views.py
+++ apps/auths/views.py
@@ -9,17 +9,6 @@ from django.contrib.auth.models import update_last_login
 from .serializers import RegistrationSerializer
 
 
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework.permissions import AllowAny, IsAuthenticated
-from rest_framework.authtoken.models import Token
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.models import update_last_login
-
-from .serializers import RegistrationSerializer
-
-
 class RegistrationView(APIView):
     permission_classes = (AllowAny,)
 
