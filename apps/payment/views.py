13d2978223073e72b2af164cd7bd3983c3c4829f
muratbek2255 <muratbek2255@gmail.com>
2023-06-17 16:11:05 +0600

    add permission for views category, payment, wallet

--- apps/payment/views.py
+++ apps/payment/views.py
@@ -1,20 +1,30 @@
-from rest_framework.mixins import CreateModelMixin, UpdateModelMixin
+from rest_framework.mixins import (
+    CreateModelMixin, UpdateModelMixin
+)
 from rest_framework.viewsets import GenericViewSet
+from rest_framework.permissions import (
+    IsAuthenticated
+)
 
 from apps.payment.models import Payment
-from apps.payment.serializers import PaymentSerializer, PaymentFinishSerializer, PaymentRollbackSerializer
+from apps.payment.serializers import (
+    PaymentSerializer, PaymentFinishSerializer, PaymentRollbackSerializer
+)
 
 
 class PaymentView(GenericViewSet, CreateModelMixin):
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
+    permission_classes = IsAuthenticated
 
 
 class PaymentFinishView(GenericViewSet, UpdateModelMixin):
     queryset = Payment.objects.all()
     serializer_class = PaymentFinishSerializer
+    permission_classes = IsAuthenticated
 
 
 class PaymentRollbackView(GenericViewSet, UpdateModelMixin):
     queryset = Payment.objects.all()
     serializer_class = PaymentRollbackSerializer
+    permission_classes = IsAuthenticated
