b9e7b16314a6d1932ff2f8c13128d2eab8b3abba
muratbek2255 <muratbek2255@gmail.com>
2023-06-17 16:43:17 +0600

    fix in views categories 3

--- apps/categories/views.py
+++ apps/categories/views.py
@@ -4,4 +4,7 @@ from apps.categories.serializers import CategorySerializer
 
 
 class CategoryView(generics.CreateAPIView):
+    permission_classes = (
+        permissions.IsAuthenticated,
+    )
     serializer_class = CategorySerializer
