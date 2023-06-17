accc7100e3382332f195d333b8c71be5e1e682f3
muratbek2255 <muratbek2255@gmail.com>
2023-06-17 16:51:19 +0600

    fix in settings 2

--- hakaton_summer/settings.py
+++ hakaton_summer/settings.py
@@ -87,9 +87,9 @@ REST_FRAMEWORK = {
 
     ],
 
-    # 'DEFAULT_PERMISSION_CLASSES': [
-    #     'rest_framework.permissions.IsAuthenticated',
-    # ]
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
 }
 
 AUTH_USER_MODEL = 'auths.Users'
